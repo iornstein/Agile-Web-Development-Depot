c296d270d8d81644aa19d34afa1ce67f7fec928f
John Ryan and Mariana Lenetis <pair+john+mariana@pivotallabs.com>
2014-06-20 17:26:53 -0700

    depot

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,2 +1,28 @@
 require 'rails_helper'
+#require 'spec_helper'
 
+describe "A Product" do
+
+  @product = Product.new(title: "A", description: "a great letter",
+                         image_url: "www.r.com", price: 4.50)
+  subject { @product }
+
+  describe "that has all attributes" do
+    puts subject
+    it { should be_valid }
+  end
+
+#  describe "that is missing an attribute" do
+#    @product.title = ""
+#    it { should_not be_valid }
+#  end
+
+#  describe "that has no attributes" do
+
+#    before { @product2 = Product.new }
+#    subject { @product2 }
+#    it { should_not be_valid }
+#  end
+
+
+end
